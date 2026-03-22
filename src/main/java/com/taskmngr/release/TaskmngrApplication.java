c8be168bb76f94ad922581f5b8783e78c78fb848
Konrad C <kcynar00@gmail.com>
2026-03-22 17:02:10 +0100

    first commit

---
+++ src/main/java/com/taskmngr/release/TaskmngrApplication.java
@@ -0,0 +1,12 @@
+package com.taskmngr.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmngrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmngrApplication.class, args);
+    }
+}
---
+++ src/main/java/com/taskmngr/release/model/Task.java
@@ -0,0 +1,28 @@
+package com.taskmngr.release.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String description;
+    
+    public Task() {}
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
